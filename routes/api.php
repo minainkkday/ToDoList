72f2026ddb385c67fbc2b92a9dfa4f9926b7194d
mina.kim <mina.kim@kkday.com>
2023-05-11 14:51:57 +0800

    Integer Constraints

--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,17 @@ use App\Http\Controllers\TodoController;
 Route::prefix('/todos')->group(function (){
     //GET
     Route::get('/', [TodoController::class, 'index']);
-    Route::get('{id}', [TodoController::class, 'details']);
+    Route::get('{id}', [TodoController::class, 'details'])->whereNumber('id');//Routing - constraints, 404 error code by default 
 
     //POST
     Route::post('/', [TodoController::class, 'store']);
 
     //PUT
-    Route::put('{id}', [TodoController::class, 'update']);
+    Route::put('{id}', [TodoController::class, 'update'])->whereNumber('id');
 
     //DELETE
-    Route::delete('{id}', [TodoController::class, 'delete']);
     Route::delete('/', [TodoController::class, 'deleteAll']);
+    Route::delete('{id}', [TodoController::class, 'delete'])->whereNumber('id');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
