ff5d5ceaffecdb89e888c4f75716b3b65a619f88
mina.kim <mina.kim@kkday.com>
2023-05-11 14:53:26 +0800

    clean code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function responseSuccess($data = null){
+        $metadata = ['status' => '0000', 'desc'=>'Success'];
+        $responseData = ['metadata' => $metadata, 'data' => $data];
+        return response()->json($responseData, 200);
+    }
+
+    public function responseFail($data = null){
+        $metadata = ['status' => '0001', 'desc'=>'Fail'];
+        $responseData = ['metadata' => $metadata, 'data' => $data];
+        return response()->json($responseData, 400);
+    }
+
+    public function responseNotfound($data = null){
+        $metadata = ['status' => '0002', 'desc'=>'Not Found'];
+        $responseData = ['metadata' => $metadata, 'data' => $data];
+        return response()->json($responseData, 404);
+    }
 }
+
--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,22 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use App\Models\Todo;
+use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
     public function index(){
         $todo = Todo::all();
-        if ($todo){
-            return response()->json(['status' => true, 'message' => "Get success", 'data' => $todo], 200);
-        }
-        
-        $messageError = "Can't get the data";
-        return response()->json(['status' => false, 'message' => $messageError, 'data' => null], 404); //Not found
-    
+        return $this->responseSuccess($todo);
     }
 
     public function store(Request $request){
         //I can specify required data
+        //感覺可以再改進，像自己設 validation rules，error message 的部分
         try{
             $request->validate([
                 'name' => 'required',
@@ -29,87 +25,80 @@ class TodoController extends Controller
         }
         catch (ValidationException $exception) {
             $errorMessages = $exception->validator->getMessageBag()->getMessages();
-            return $errorMessages;
+            return response()->json($errorMessages);
         }
-        //try catch -> 為了執行後面的流程
-        //error handle -》 改 json 的 format 來改緊啊
 
         $data = $request->all();//取得所有input
-        
 
         $todo = new Todo();//OOP - 呼叫 model
         $todo->name = $data['name'];
         $todo->description = $data['description'];
-        
-        //fail comes first， ans success comes last
-        if ($todo->save()){
-            return response()->json(['status' => true, 'message' => "Create success!"], 201);
+
+        $saved = $todo->save();
+
+        if(!$saved){
+            return $this->responseFail();
         }
-        else{
-            $messageError = "Fail to create.";
-            return response()->json(['status' => false, 'message' => $messageError], 400); 
-        }
+
+        return $this->responseSuccess($todo);
     }
 
 
     public function details($id){
-        // $todo = Todo::findOrFail($id); //throw 404 not found error
         $todo = Todo::find($id);
-        if ($todo){
-            return response()->json(['status' => true, 'message' => "Get details success", 'data' => $todo], 200);
+
+        //Fail to find a data
+        if(!$todo){
+            return $this->responseNotfound();
         }
-        else{
-            $messageError = "Can't find the data";
-            return response()->json(['status' => false, 'message' => $messageError, 'data' => null], 404);//not found 
-        }
+
+        return $this->responseSuccess($todo);
     }
-    
-    // public function edit(){
-    //     return view('edit');
-    // }
+
 
     public function update(Request $request, $id)
     {
-        $todo = Todo::findOrFail($id);//405 not found error, so it will not process upcoming requests
-        //need to validate integer type, SQL injection - required
-        //url 上面的寫法， input 進來的 parameter， 可以做到多細 是我能決定的部分
+        $todo = Todo::find($id);
 
+        if(!$todo){
+            return $this->responseNotfound();
+        }
+        
         $data = $request->all();
         
         $todo->name = $data['name'];
         $todo->description = $data['description'];
 
-        if ($todo->save()){
-            return response()->json(['status' => true, 'message' => "Update success!"], 200);
+        $saved = $todo->save();
+
+        if(!$saved){
+            return $this->responseFail();
         }
-        else{
-            $messageError = "Fail to update.";
-            return response()->json(['status' => false, 'message' => $messageError], 400); 
-        }
+
+        return $this->responseSuccess($todo);
     }
 
     public function delete($id){
+        $todo = Todo::find($id);
+        if(!$todo){
+            return $this->responseNotfound();
+        }
+        
+        $deleted = $todo->delete();
 
-        $todo = Todo::findOrFail($id);
- 
-        if ($todo->delete()){
-            return response()->json(['status' => true, 'message' => "Delete success!"], 204);
+        if (!$deleted){
+            return $this->responseFail();
         }
-        else{
-            $messageError = "Fail to delete";
-            return response()->json(['status' => false, 'message' => $messageError, 'data' => null], 400); 
-        }
+
+        return $this->responseSuccess();
     }
 
     public function deleteAll(){
- 
-        if (Todo::truncate()){
-            return response()->json(['status' => true, 'message' => "Delete success!"], 204);
-        }
-        else{
-            $messageError = "Fail to delete";
-            return response()->json(['status' => false, 'message' => $messageError, 'data' => null], 400); 
+        $deleted = Todo::truncate();
+        if (!$deleted){
+            return $this->responseFail(); 
         }
 
+        return $this->responseSuccess();
     }
 }
