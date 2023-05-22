f7a9328a72fb204d15e79e8e7afb087a084ca34d
mina.kim <mina.kim@kkday.com>
2023-05-22 09:54:15 +0800

    Update by feedback

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    // protected $fillable = ['name','description'];//可填 or 不可填
+    protected $fillable = ['user_id','name','description'];//可填 or 不可填
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\AuthController;
 
+Route::prefix('todos')->group(function (){
+    //GET
+    Route::get('/', [TodoController::class, 'index']);
+    Route::get('/{id}', [TodoController::class, 'details'])->whereNumber('id');//Routing - constraints, 404 error code by default 
+
+    //POST
+    Route::post('/', [TodoController::class, 'store']);
+
+    //PUT
+    Route::put('/{id}', [TodoController::class, 'update'])->whereNumber('id');
+
+    //DELETE
+    Route::delete('/', [TodoController::class, 'deleteAll']);
+    Route::delete('/{id}', [TodoController::class, 'delete'])->whereNumber('id');
+});
+
 Route::prefix('/users')->group(function (){
     //GET
     Route::get('/register', [AuthController::class, 'seeRegistered']);
@@ -14,22 +30,6 @@ Route::prefix('/users')->group(function (){
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']); 
     Route::post('/logout', [AuthController::class, 'logout']); 
-    
-    Route::prefix('todos')->group(function (){
-        //GET
-        Route::get('/', [TodoController::class, 'index']);
-        Route::get('/{id}', [TodoController::class, 'details'])->whereNumber('id');//Routing - constraints, 404 error code by default 
-    
-        //POST
-        Route::post('/', [TodoController::class, 'store']);
-    
-        //PUT
-        Route::put('/{id}', [TodoController::class, 'update'])->whereNumber('id');
-    
-        //DELETE
-        Route::delete('/', [TodoController::class, 'deleteAll']);
-        Route::delete('/{id}', [TodoController::class, 'delete'])->whereNumber('id');
-    });
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
